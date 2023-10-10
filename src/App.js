6a3674410cb29602aded1f1aa86a259c1df09cb1
Menna-T-ullah <53189714+Menna-T-ullah@users.noreply.github.com>
2023-10-10 18:20:21 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
+import { SocialIcon } from "react-social-icons";
+import { FacebookShareButton, FacebookIcon } from "react-share";
+import { TwitterShareButton, TwitterIcon } from 'react-share';
 
 const App = () => {
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
 
   return (
     <>
@@ -30,13 +35,47 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
         </div>
+        <SocialIcon
+          network="twitter"
+          bgColor="#262834"
+          fgColor="#56cbdb"
+          className="social"
+          onClick={() =>
+            window.open(
+              `https://twitter.com/compose/tweet?text=${quote.content} - ${quote.author}`
+            )
+          }
+        />
+        <SocialIcon
+          network="whatsapp"
+          bgColor="#262834"
+          fgColor="#56cbdb"
+          className="social"
+          onClick={() =>
+            window.open(
+              `https://web.whatsapp.com/send?text=${quote.content} - ${quote.author}`
+            )
+          }
+        />
+        <SocialIcon
+          network="telegram"
+          bgColor="#262834"
+          fgColor="#56cbdb"
+          className="social"
+          onClick={() =>
+            window.open(
+              `https://telegram.me/share/url?url=${quote.content} - ${quote.author}`
+            )
+          }
+        />
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
